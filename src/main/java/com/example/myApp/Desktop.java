923439be88002397d9050c252247b6f237644a59
sandeepmedikonda <sandeepmedikonda110@gmail.com>
2024-11-13 11:56:32 +0530

    Added my first

---
+++ src/main/java/com/example/myApp/Desktop.java
@@ -0,0 +1,11 @@
+package com.example.myApp;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Desktop implements Computer {
+    public void compile() {
+
+        System.out.println("Compiling with 404 bugs but faster");
+    }
+}
